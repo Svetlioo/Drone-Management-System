f6b01ec8242795d8aeba72a12a110043e08c1d33
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2024-02-26 01:45:56 +0200

    Arrange all components in App.jsx

--- README.md
+++ README.md
@@ -7,3 +7,50 @@ Svetoslav Iliev
 ### email
 
 svetlio.iliev17@gmail.com
+
+# Drone Management System
+
+### **Important: Running the Project Locally**
+
+Before diving into the features and details of this project, it's important to know how to get it running on your local machine:
+
+- Clone this repo
+- `npm install` to install all required dependencies
+- **`npm run server` to start the local server**
+- **`npm run dev` to start the development server**
+
+Once the server is running, open your browser and visit `http://localhost:<your_port>` to start using the application.
+
+Replace `<your_port>` with the port number your server is running on. If you didn't set a specific port, it's likely running on port 3000, so you would visit `http://localhost:3000`.
+
+Now, let's dive into what this project is all about.
+
+## Project Overview
+
+This project is a real-time drone management system that simulates drone movement in a grid environment. It calculates and displays the distance, path, and number of steps between two points on the grid, supporting both diagonal and non-diagonal movements.
+
+The system is built with WebSockets, allowing for real-time updates and concurrent users. Users can add orders through a form and these orders are updated in real-time across all connected clients. The system constantly keeps every user updated about the whole state of the entire simulation process.
+
+Furthermore, the project is designed with extensibility in mind, allowing for seamless integration of additional features in the future. Although certain features were not implemented due to time constraints, the modular architecture and robust algorithms provide a solid foundation for further development and enhancement.
+
+As a Java developer, working solely with JavaScript on this project presented a real challenge. The lack of type safety in JavaScript compared to Java (in this case TypeScript) made the development process slower and more error-prone. However, this challenge provided an opportunity for me to enhance my skills and knowledge in JavaScript development, adapting to a dynamically-typed language.
+
+Debugging was particularly difficult due to the complex nature of the project. To overcome this challenge, I made a conscious effort to thoroughly analyze and familiarize myself with every aspect of the codebase. By gaining a deep understanding of the project's architecture, algorithms, and implementation details, I was able to effectively identify and fix issues, making the debugging process more manageable.
+
+This experience not only improved my debugging skills but also allowed me to become more proficient in JavaScript development. It reinforced the importance of thorough understanding and attention to detail in software development, especially when working with complex projects.
+
+## Features
+
+- Real-time drone movement simulation in a grid environment with a legend.
+- Calculation and display of distance, path, and number of steps between two destinations.
+- Support for both diagonal and non-diagonal movements.
+- Real-time updates and support for concurrent users through WebSockets.
+- User orders can be added through a form and are updated in real-time.
+
+## Conclusion
+
+Thank you for taking the time to explore this project. Your interest and feedback are greatly appreciated. I hope you find the work I've done here valuable and enjoyable.
+
+Best Regards,
+
+**Svetoslav Iliev**
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Grid from "./models/Grid";
-import { json5 as json } from "../src/assets/data";
-import {
-  addChargingStation,
-  addCustomer,
-  addDrone,
-  addEntitiesToGrid,
-  addWarehouse,
-} from "../src/services/GridOperationsService";
-import DeliveryTimeService from "../src/services/DeliveryTimeService";
+import { addEntitiesToGrid } from "../src/services/GridOperationsService";
 import GridCanvas from "./components/GridCanvas";
 import Legend from "./components/Legend";
 import FileInput from "./components/FileInput";
 import { onUpdate, sendOrder } from "./services/socketService";
+import SimulationInfo from "./components/SimulationInfo";
+import Order from "./models/Order";
+import OrderForm from "./components/OrderForm";
+import DeliveryTime from "./components/DeliveryTime";
+import GridMovementTest from "./components/GridMovementTest";
 
 function App() {
   const [data, setData] = useState(null);
@@ -39,15 +36,6 @@ function App() {
     fetchData();
   }, [data]);
 
-  const deliveryTimeService = new DeliveryTimeService();
-  console.log(
-    deliveryTimeService.calculateTotalDeliveryTime(
-      json.warehouses,
-      json.customers,
-      json.orders
-    )
-  );
-
   if (!gridData) {
     return <h1>Loading...</h1>;
   }
@@ -58,56 +46,15 @@ function App() {
         <GridCanvas grid={grid} gridData={gridData} cellSize={10} />
       </div>
       <Legend />
-
       <FileInput />
-
-      <h2>{JSON.stringify(data.simulationState.orders)}</h2>
-
-      <button
-        style={{ padding: "5px 10px", borderRadius: "5px", border: "none" }}
-        onClick={() => {
-          console.log("Adding warehouses and customers");
-          addWarehouse(grid, setGridData, 1, 1);
-          addCustomer(grid, setGridData, 2, 2);
-          addChargingStation(grid, setGridData, 3, 3, "normal");
-          addDrone(grid, setGridData, 5, 5, "500W", "1000W", "normal");
-          sendOrder({ id: 14, product: "Product Name", quantity: 23 });
-        }}
-      >
-        Add warehouses and customers
-      </button>
-      {/* 
-      <h2>Diagonal movement enabled.</h2>
-      <div>Distance: {grid.calculateDistance(200, 200, 216, 187)}</div>
-      <div>Path: {JSON.stringify(grid.calculatePath(200, 200, 216, 187))}</div>
-      <div>
-        Number of steps: {grid.calculateNumberOfSteps(200, 200, 216, 187)}
-      </div>
-
-      <h2>No diagonal movement.</h2>
-      <div>Distance: {grid.calculateDistance(10, 10, 100, 100)}</div>
-      <div>
-        Path:{" "}
-        {JSON.stringify(grid.calculatePathWithoutDiagonal(10, 10, 100, 100))}
-      </div>
-      <div>
-        Number of steps:{" "}
-        {grid.calculateNumberOfStepsWithoutDiagonal(10, 10, 100, 100)}
-      </div> */}
-      {/* <h2>Diagonal movement enabled.</h2>
-      <div>Distance: {grid.calculateDistance(1, 1, 1, 30)}</div>
-      <div>Path: {JSON.stringify(grid.calculatePath(1, 1, 1, 30))}</div>
-      <div>Number of steps: {grid.calculateNumberOfSteps(1, 1, 1, 30)}</div>
-
-      <h2>No diagonal movement.</h2>
-      <div>Distance: {grid.calculateDistance(1, 1, 1, 30)}</div>
-      <div>
-        Path: {JSON.stringify(grid.calculatePathWithoutDiagonal(1, 1, 1, 30))}
-      </div>
-      <div>
-        Number of steps:{" "}
-        {grid.calculateNumberOfStepsWithoutDiagonal(1, 1, 1, 30)}
-      </div> */}
+      <DeliveryTime data={data} />
+      <OrderForm
+        sendOrder={sendOrder}
+        Order={Order}
+        customers={data.simulationState.customers}
+      />
+      <SimulationInfo data={data.simulationState} />
+      <GridMovementTest grid={grid} />
     </>
   );
 }
